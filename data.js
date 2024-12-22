5ff4566342ae3a57ae67784a75967bf973469cef
dv-mahbub <dv.mahbub@gmail.com>
2024-12-22 12:46:57 +0600

    custom module

---
+++ data.js
@@ -0,0 +1,5 @@
+const name = "John Doe";
+const age = 30;
+const city = "Dhaka";
+const hobbies = ["reading", "traveling", "swimming"];
+export default { name, age, city, hobbies };
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 console.log("hello world!");
 console.log(1 + 3);
+
+let a = 5;
+var b = 10;
+
+console.log(a + b);
+
+// console.log(global.fetch);
+
+const data = require("./data").default;
+console.log(data.name);
+console.log(data.hobbies[1]);
+console.log(data.hobbies[1][2]);
