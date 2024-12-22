2caac3073e1794b617a30038628c0d3c1680b674
dv-mahbub <dv.mahbub@gmail.com>
2024-12-22 12:59:25 +0600

    used combine modules

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const data = require("./data").default;
+
 console.log("hello world!");
 console.log(1 + 3);
 
@@ -8,22 +11,26 @@ console.log(a + b);
 
 // console.log(global.fetch);
 
-const data = require("./data").default;
 console.log(data.name);
 console.log(data.hobbies[1]);
 console.log(data.hobbies[1][2]);
 
-const _ = require("lodash");
-
 const array = [1, 2, 3, 4, 5];
 const doubledArray = _.map(array, (num) => num * 2);
-
 const tripledArray = _.map(array, (a) => a * 3);
+
 console.log(doubledArray);
 console.log(tripledArray);
 
 const stringArray = ["a", "b", "c", "d"];
 const upperCaseArray = _.map(stringArray, (str) => str.toUpperCase());
 const repeatedArray = _.map(stringArray, (str) => str.repeat(3));
+
 console.log(upperCaseArray);
 console.log(repeatedArray);
+
+console.log(_.random(1, 100));
+console.log(_.last(repeatedArray));
+console.log(_.lastIndexOf(repeatedArray));
+
+console.log(data.hobbies[_.lastIndexOf(data.hobbies) - 1]);
