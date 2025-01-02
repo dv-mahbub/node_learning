75dee84fd9bf1a255ca50ad9d08639cb564ca6d4
dv-mahbub <dv.mahbub@gmail.com>
2025-01-02 11:43:15 +0600

    router level middleware

---
+++ express/middleware/router-level-middleware.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const router = express.Router();
+
+app.use("/admin", router);
+
+const logger = (req, res, next) => {
+  console.log(`Original url: ${req.originalUrl}`);
+  next();
+};
+
+router.use(logger);
+
+app.get("/about", (req, res) => {
+  res.send("App about");
+});
+router.get("/about", (req, res) => {
+  res.send("Router about");
+});
+
+app.listen(4000, () => {
+  console.log("App is listenning at 4000");
+});
