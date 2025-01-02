7c330947e4b6345cfc5ed0e5f673ed9d8eda33c4
dv-mahbub <dv.mahbub@gmail.com>
2025-01-02 11:31:13 +0600

    application level middleware

---
+++ express/middleware/app-level-middleware.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Time: ${new Date(Date.now()).toLocaleString()}`);
+  //   res.end();
+  //   res.send("Home section");
+  next();
+};
+
+const contactDetails = null;
+
+const loggerMiddleware2 = (req, res, n) => {
+  console.log("2ns middleware");
+  if (req.path === "/contact" && !contactDetails) {
+    res.send("No contact Details");
+    return;
+  }
+  n();
+};
+
+app.use(loggerMiddleware);
+app.use(loggerMiddleware2);
+
+app.get("/about", (req, res) => {
+  res.send("This is about section");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact details");
+});
+
+app.listen(4000, () => {
+  console.log("App is listenning at 4000");
+});
